dbc5911c0858f1b6b8483d913c7c9c2050a17bb9
joonoaspdosa <aspdosa@gmail.com>
2022-09-20 14:30:03 +0900

    프론트 엔드 개발

---
+++ add_two_numbers.js
@@ -0,0 +1,9 @@
+function doSomething(){
+    let num1 = document.getElementById("inputA").value;
+    let num2 = document.getElementById("inputB").value;
+
+    document.getElementById("valueA").innerHTML = num1;
+    document.getElementById("valueB").innerHTML = num2;
+    /*document.getElementById("valueC").innerHTML = num1 + num2;*/
+    document.getElementById("valueC").innerHTML = Number(num1) + Number(num2);
+}
